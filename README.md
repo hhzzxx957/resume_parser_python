2dae2e12894d09a79eb0c2026bba4e929bf27abd
hhzzxx957 <hzx957@vt.edu>
2020-06-02 13:50:37 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Resume parser Python
---
+++ driver.py
@@ -0,0 +1,8 @@
+from pprint import pprint
+from resparser import ResumeParser
+
+# data1 = ResumeParser('resume/Resume_Jason(ZhixingHe).pdf').get_extracted_data()
+# pprint(data1)
+
+data2 = ResumeParser('resume/OmkarResume.pdf').get_extracted_data()
+pprint(data2)
